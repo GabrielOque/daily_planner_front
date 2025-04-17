6dbc9727314c68ecaa1704b25019ae6ffef39745
GabrielOque <oquendodev@gmail.com>
2025-04-16 20:25:51 -0500

    Test push without origin

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="w-1/4 2xl:w-1/5 h-full rounded-2xl bg-sideBar p-4">
-      <h1 className="text-2xl font-bold text-neutral">Daily Planner</h1>
+      <h1 className="text-2xl font-bold text-neutral">Daily Planner Gab</h1>
     </div>
   );
 };
