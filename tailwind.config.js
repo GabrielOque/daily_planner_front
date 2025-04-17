9fde6acc8263441e4e13d36db435740113566e97
GabrielOque <oquendodev@gmail.com>
2025-04-16 20:24:36 -0500

    Refactor styles and update assets
    - Updated favicon.ico with a new design.
    - Removed unused font files: GeistMonoVF.woff and GeistVF.woff.
    - Cleaned up globals.css by removing custom CSS variables and styles for background and text colors.
    - Enhanced tailwind.config.js with new color variables for improved theming and UI consistency.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        neutral: "#111111", // Fondo principal oscuro
+        neutralLight: "#F6F6F6", // Fondos de tarjetas y áreas secundarias
+        neutralLighter: "#F4F4F4", // Sidebar y secciones de apoyo
+        border: "#EAEAEA", // Bordes y líneas separadoras
+        muted: "#E5E7EB", // Elementos deshabilitados o secundarios
+        sideBar: "#D9D9D9", // Fondo de la barra lateral
+
+        textPrimary: "#FFFFFF", // Texto principal
+        textSecondary: "#444444ff", // Texto secundario o deshabilitado
+        textContrast: "#111111", // Texto en fondos claros
+
+        primary: "#FDC938", // Amarillo (botón "Get Started", resaltes clave)
+        primaryDark: "#D9A600", // Amarillo más oscuro para hover
+
+        accentRed: "#FC5C45", // Rojo para alertas o tags destacados
+        accentBlue: "#91D8E4", // Azul claro para calendario o info secundaria
+        accentYellow: "#FFD166", // Amarillo complementario
+
+        tagSuccess: "#B2F5EA", // Verde agua para tareas completadas o positivas
+        tagWarning: "#FECACA", // Rojo suave para alertas o tareas pendientes
       },
     },
   },
