3563eb34e74c08f4d8987a91a0be7817ba6a8a73
GabrielOque <oquendodev@gmail.com>
2025-04-16 22:10:34 -0500

    Update button text in Home component

--- src/app/planner/page.js
+++ src/app/planner/page.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <button onClick={() => router.push("/planner/calendar")}>
-        Home principal
+        Home principal updated by Gabriel
       </button>
     </div>
   );
