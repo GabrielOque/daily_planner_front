b131aaf20463aef2906b6f6c5e52a7683a6e970d
GabrielOque <oquendodev@gmail.com>
2025-04-18 13:08:44 -0500

    Implement token validation and loading state in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,52 @@
 "use client";
-
+import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
+import { getToken } from "@/app/utils/auth";
+import axios from "@/app/utils/axiosInstance";
+import { removeToken, setToken } from "@/app/utils/auth";
+import { logoutUser, setUserAuth } from "@/app/store/features/user/userSlice";
 
 //Components
 import Button from "@/components/Button";
+import { useEffect, useState } from "react";
 import AuthWrapper from "@/components/AuthWrapper";
+
+import { NEXT_PUBLIC_API_URL } from "@/app/utils/envConfig";
 
 export default function Home() {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  // Check token
+  const checkToken = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `${NEXT_PUBLIC_API_URL}/user/check-token`
+      );
+      if (response.status === 200) {
+        setToken(response.data.token);
+        dispatch(setUserAuth(response.data));
+        setLoading(false);
+        router.push("/planner");
+      }
+    } catch (error) {
+      setLoading(false);
+      removeToken();
+      dispatch(logoutUser());
+    }
+  };
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      checkToken();
+    }
+  }, []);
+
   return (
-    <AuthWrapper>
+    <AuthWrapper loading={loading}>
       <h1 className="text-neutral font-bold text-4xl">Mente productiva</h1>
       <p className="text-textSecondary font-semibold pt-6 text-xl px-12 2xl:px-36">
         Con solo estas funciones, Daily Planner está personalizado para personas
