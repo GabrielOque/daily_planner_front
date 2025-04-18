48ea7f3b54a29b3c03de3256a434b874487ba164
GabrielOque <oquendodev@gmail.com>
2025-04-18 13:08:34 -0500

    Wrap RootLayout component with ProviderStore for state management

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import ProviderStore from "@/components/ProviderStore";
 
 export const metadata = {
   title: "Daily Planner",
@@ -8,18 +9,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <body className="antialiased bg-gray-100 h-screen flex flex-col w-full">
-        <ToastContainer />
-        {children}
-      </body>
-    </html>
+    <ProviderStore>
+      <html lang="en">
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+            crossOrigin="anonymous"
+          />
+        </head>
+        <body className="antialiased bg-gray-100 h-screen flex flex-col w-full">
+          <ToastContainer />
+          {children}
+        </body>
+      </html>
+    </ProviderStore>
   );
 }
