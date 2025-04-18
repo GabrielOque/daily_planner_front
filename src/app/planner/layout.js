85f2a370e7178392415692a2d4ad13480768a7d9
GabrielOque <oquendodev@gmail.com>
2025-04-18 13:09:03 -0500

    Wrap Planner component with ProtectedRoutes for enhanced route protection

--- src/app/planner/layout.js
+++ src/app/planner/layout.js
@@ -1,14 +1,17 @@
 "use client";
 import Sidebar from "@/components/Sidebar";
+import ProtectedRoutes from "@/components/ProtectedRoutes";
 export default function Planner({ children }) {
   return (
-    <div className="h-screen flex flex-col w-full">
-      <div className="w-full h-full flex justify-between p-8 gap-x-4">
-        <Sidebar />
-        <div className="w-3/4 2xl:w-4/5 h-full border border-sideBar rounded-2xl p-4">
-          {children}
+    <ProtectedRoutes>
+      <div className="h-screen flex flex-col w-full">
+        <div className="w-full h-full flex justify-between p-8 gap-x-4">
+          <Sidebar />
+          <div className="w-3/4 2xl:w-4/5 h-full border border-sideBar rounded-2xl p-4">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </ProtectedRoutes>
   );
 }
