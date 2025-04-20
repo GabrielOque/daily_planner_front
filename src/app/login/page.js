72047eaa460311394f42213d5ccf3090acbd8a82
GabrielOque <oquendodev@gmail.com>
2025-04-19 23:25:27 -0500

    Remove commented fast navigation button from Login component

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -43,7 +43,7 @@ const Login = () => {
   return (
     <AuthWrapper>
       <h1 className="text-neutral font-bold text-4xl">Iniciar sesión</h1>
-      <button onClick={() => router.push("/planner")}>Fast go</button>
+      {/* <button onClick={() => router.push("/planner")}>Fast go</button> */}
       <div className="w-full px-12 2xl:px-40 pt-6 space-y-3">
         <CustomInput
           placeholder="oquendogabriel18@gmail.com"
