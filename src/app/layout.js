b35f456b75c3f6c0bf47b49fa39aff3f97515d75
GabrielOque <oquendodev@gmail.com>
2025-05-11 19:46:57 -0500

    fix: comment out unused Font Awesome stylesheet link and ensure script tag is properly formatted

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,11 +12,15 @@ export default function RootLayout({ children }) {
     <ProviderStore>
       <html lang="es">
         <head>
-          <link
+          {/* <link
             rel="stylesheet"
             href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
             crossOrigin="anonymous"
-          />
+          /> */}
+          <script
+            src="https://kit.fontawesome.com/48de85dc46.js"
+            crossOrigin="anonymous"
+          ></script>
         </head>
         <body className="antialiased bg-background h-screen flex flex-col w-full">
           <ToastContainer />
